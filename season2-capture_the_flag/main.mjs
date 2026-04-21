af03f5c4864b85f1250ab9d8afa3aa306d3d5c39
nessab35 <nessa_begay.35@icloud.com>
2026-04-21 16:40:31 -0500

    step 1: move bots

--- season2-capture_the_flag/main.mjs
+++ season2-capture_the_flag/main.mjs
@@ -1,8 +1,12 @@
-import { } from 'game/utils';
-import { } from 'game/prototypes';
-import { } from 'game/constants';
-import { } from 'arena/season_beta/capture_the_flag/basic';
+import { getObjectsByPrototype } from "game/utils";
+import { Creep, Flag } from "game/prototypes";
+// import {} from "game/constants";
+// import {} from "arena/season_beta/capture_the_flag/basic";
 
 export function loop() {
-    // Your code goes here
+	var enemyFlag = getObjectsByPrototype(Flag).find((object) => !object.my);
+	var myCreeps = getObjectsByPrototype(Creep).filter((object) => object.my);
+	for (var creep of myCreeps) {
+		creep.moveTo(enemyFlag);
+	}
 }
